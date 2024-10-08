c44fd159ccf5dd36a05ea139d50340ab352906ea
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-08 15:22:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi JSON</title>
+
+    <!--Style-->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <div id="app">
+        <h1>php-dischi-json</h1>
+        <h2>{{message}}</h2>
+    </div>
+    <!--VUE-->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!--JS-->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
